6c7a4d3406c1d7a46f15462b18b98703e10fcb29
dannyliu <bobobo746@hotmail.com>
2018-08-30 14:43:05 +0800

    v2 , basic blockchain concept  accomplished.

--- DNABlockChain/DNABlockChain/urls.py
+++ DNABlockChain/DNABlockChain/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include, url
 from django.urls import path
 
+from miner import views
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
+    url(r'^mine', views.mine),
+    url(r'^transactions/new', views.new_transaction),
+    url(r'^chain', views.full_chain),
+
 ]
--- DNABlockChain/miner/views.py
+++ DNABlockChain/miner/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 import hashlib
 import json
@@ -68,7 +69,7 @@ class Blockchain:
 
 
 
-#########  #########   #########   #########  #########   #########   #########  #########  #########
+#########  #########
 
 
 
@@ -80,8 +81,53 @@ node_identifier = str(uuid4()).replace('-','')
 blockchain = Blockchain()
 
 
+@csrf_exempt
+def new_transaction(request):
+    values = json.loads(request.body.decode('utf8'))
+    required = ['sender','recipient','amount']
+    if not all(k in values for k in required):
+        return 'Missing Values'
+
+    index = blockchain.new_transaction(values['sender'],values['recipient'],values['amount'])
+    print(index)
+
+    response  ={'message': 'Transaction will be added to Block %s'%index}
+
+    return HttpResponse(json.dumps(response))
+
+
+@csrf_exempt
 def mine(request):
 
-    return HttpResponse()
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+
+    proof = blockchain.proof_work(last_proof)
+    print(proof)
+
+    blockchain.new_transaction(
+        sender ='0',
+        recipient= node_identifier,
+        amount=1,
+    )
+
+    block = blockchain.new_block(proof)
+    response = {
+        'message':'New Block Forged',
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof':block['proof'],
+        'previous_hash': block['prevoius_hash'],
+    }
+    print(response)
+
+    return HttpResponse(json.dumps(response))
 
 
+@csrf_exempt
+def full_chain(request):
+    response ={
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return HttpResponse(json.dumps(response))
